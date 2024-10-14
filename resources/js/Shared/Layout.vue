6d338fd0a794237e752dec3f45b500a76ce0526b
koko kyaw <kokokyaw@myansis.com>
2024-10-15 01:35:50 +0700

    completed shared_data

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,9 +1,15 @@
 <template>
     <section class="p-6 bg-gray-200">
-        <header class="flex">
-            <h1 class="font-bold text-lg">
-                My App
-            </h1>
+        <header class="flex justify-between">
+            <div class="flex items-center">
+                <h1 class="font-bold text-lg">
+                    My App
+                </h1>
+
+                <p class="text-sm ml-4">
+                    Welcome back, {{ username }}!
+                </p>
+            </div>
 
             <Nav />
         </header>
@@ -20,6 +26,12 @@ import Nav from "./Nav"
 export default {
     components: {
         Nav
+    },
+
+    computed: {
+        username() {
+            return this.$page.props.auth.user.username
+        }
     }
 }
 </script>
